91c5065cbc1927e0f456f211214a167611aa2312
Michael Regert <mjr171@g.harvard.edu>
2016-11-30 21:41:17 -0600

    Got first one-to-many DB migration and seeder working

--- database/seeds/ReviewsTableSeeder.php
+++ database/seeds/ReviewsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Campground;
+
 
 class ReviewsTableSeeder extends Seeder
 {
@@ -11,12 +13,15 @@ class ReviewsTableSeeder extends Seeder
      */
     public function run()
     {
+        $campground_id = Campground::where('name','=','LJB Ranch')->pluck('id')->first();
+
         DB::table('reviews')->insert([
             'created_at' => Carbon\Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
             'username' => 'Codemonkey_42',
-            'review' => 'This campground is awesome.  It has a great firepit for roasting marshmallows and it is FREE for all scouts',,
-            'star_raing' => 5,
+            'review' => 'This campground is awesome.  It has a great firepit for roasting marshmallows and it is FREE for all scouts',
+            'star_rating' => 5,
+            'campground_id' => $campground_id,
         ]);
     }
 }
